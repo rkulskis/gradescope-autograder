1a5809b2e4d81430412a2e29b2c053dab3e4d193
rkulskis <rkulskis@gmail.com>
2023-11-04 21:45:37 +0000

    gradescope runs locally

--- source/run_tests.py
+++ source/run_tests.py
@@ -3,5 +3,5 @@ from gradescope_utils.autograder_utils.json_test_runner import JSONTestRunner
 
 if __name__ == '__main__':
     suite = unittest.defaultTestLoader.discover('tests')
-    with open('/autograder/results/results.json', 'w') as f:
+    with open('../results/results.json', 'w') as f:
         JSONTestRunner(visibility='visible', stream=f).run(suite)
