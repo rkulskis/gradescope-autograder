cd7d9aca8283aba63f78936c922c8352d0ff63bd
rkulskis <rkulskis@gmail.com>
2023-11-07 13:59:41 -0500

    removed unecessary files

--- zip.sh
+++ zip.sh
@@ -3,4 +3,4 @@
 # exlude all __MACOSX__ files by matching if being with _
 # exclude all . files (e.g. .git)
 # exclude all .zip files
-zip -r grader.zip . -x .\* -x _\* -x \*.zip -x zip.sh -x calculator.py
+zip -r autograder.zip . -x .\* -x _\* -x \*.zip -x zip.sh -x calculator.py
