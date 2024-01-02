99dbfd591e39b67b6729cc484418b43cd110d1fd
rkulskis <rkulskis@gmail.com>
2024-01-02 11:44:48 -0500

    gradescope autograder

---
+++ gradescope_autograder/zip.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#
+# exlude all __MACOSX__ files by matching if being with _
+# exclude all . files (e.g. .git)
+# exclude all .zip files
+zip -r autograder.zip . -x .\* -x _\* -x \*.zip -x zip.sh -x calculator.py
